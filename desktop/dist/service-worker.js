ee2a6d4c390eb4f59c0d1c3f33e26a218410ea34
geora <70815961+agrmonitorcgm@users.noreply.github.com>
2026-09-16 21:08:48 +0300

    Add automatic beer label lookup

--- desktop/dist/service-worker.js
+++ desktop/dist/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='beer-diary-v1';
+const CACHE_NAME='beer-diary-v2';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
